08e2c8a7a68fb19a2d4521fc476de9284d0c6b06
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2026-04-18 11:54:15 +0530

    chore: remove web

--- web/src/lib/auth-client.ts
+++
@@ -1,8 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_BETTER_AUTH_URL || "http://localhost:5000",
-    basePath: "/api/auth",
-});
-
-export const { signIn, signOut, getSession, useSession } = authClient;
